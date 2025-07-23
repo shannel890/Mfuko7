8505f00751e122b6172fe4dcad0b51c9f5b384b8
shannel890 <shannelkirui739@gmail.com>
2025-07-23 12:27:54 +0300

    modified the run.py file and vercel.json

--- run.py
+++ run.py
@@ -2,6 +2,5 @@ import os
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 from app import create_app
-app = create_app()
-if __name__ == "__main__":
-    app.run()
+
+app = create_app()  # Vercel will detect this WSGI app
