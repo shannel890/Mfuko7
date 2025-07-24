014121c1789b45c19c190d7d4a69889811db9b73
shannel890 <shannelkirui739@gmail.com>
2025-07-24 23:36:50 +0300

    modified models.py

--- app/models.py
+++ app/models.py
@@ -26,6 +26,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(255), unique=True, nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=True)
+    role = db.Column(db.String, nullable=False)
     is_oauth_user = db.Column(db.Boolean, default=False, nullable=False) 
     active = db.Column(db.Boolean(), default=True)
     fs_uniquifier = db.Column(db.String(64), unique=True, nullable=False)
