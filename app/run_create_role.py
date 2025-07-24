585d235edb104627eacaa065ce904f260d40e221
shannel890 <shannelkirui739@gmail.com>
2025-07-24 18:14:07 +0300

    added run_create_role.py

---
+++ app/run_create_role.py
@@ -0,0 +1,10 @@
+# run_create_role.py
+from app import create_app, db
+from app.models import Role
+
+app = create_app()
+with app.app_context():
+    role = Role(name='Tenant')
+    db.session.add(role)
+    db.session.commit()
+    print("Tenant role created.")
